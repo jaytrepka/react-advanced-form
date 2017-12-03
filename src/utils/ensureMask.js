380696db0cfa2c246fc16764b2310181ac10917a
Artem Zakharchenko <kettanaito@gmail.com>
2017-12-03 21:38:49 +0100

    Utils (ensureMask): Measure the length of stripped mask properly

--- src/utils/ensureMask.js
+++ src/utils/ensureMask.js
@@ -8,7 +8,7 @@ export default function ensureMask(string, mask) {
   const forcedString = string.toString().replace(/\W+/g, '');
 
   /* Get the overlay - the part of the string which goes beyond the mask */
-  const overlay = forcedString.substr(mask.replace(/\s+/g, '').length, forcedString.length);
+  const overlay = forcedString.substr(mask.replace(/[^#]+/g, '').length, forcedString.length);
 
   /* Replace the hashes in the mask with the characters from the string, appending an overlay */
   return mask.replace(/#/gi, (a, b, c) => forcedString[i++] || '').trim() + overlay;
--- test/unit/utils.spec.js
+++ test/unit/utils.spec.js
@@ -10,12 +10,13 @@ describe('Utils', () => {
     it ('ensureMask', () => {
       /* String and mask equal length */
       expect(ensureMask('abcdefg', '## ### ##')).to.equal('ab cde fg');
+      expect(ensureMask('12061992', '## / ## / ####')).to.equal('12 / 06 / 1992');
 
       /* String shorter than the mask */
       expect(ensureMask('1234567890', '#### #### #### ####')).to.equal('1234 5678 90');
 
       /* String longer than the mask */
-      expect(ensureMask('1234567890', '## ## ##')).to.equal('12 34 567890');
+      expect(ensureMask('1234567890', '##-##-##')).to.equal('12-34-567890');
 
       /* String with non-linear character indexes */
       expect(ensureMask('12 34567 89', '### ### ###')).to.equal('123 456 789');
